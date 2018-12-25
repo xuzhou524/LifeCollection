baee3fee487879f59e77e7e2d87859defa12e796
xuzhou524 <xuuohz@163.com>
2018-12-25 15:26:45 +0800

    bak

--- LifeCollection/Controller/LCWebViewViewController.m
+++ LifeCollection/Controller/LCWebViewViewController.m
@@ -8,25 +8,46 @@
 
 #import "LCWebViewViewController.h"
 
-@interface LCWebViewViewController ()
-
+@interface LCWebViewViewController ()<UIWebViewDelegate>
+@property(nonatomic,strong)UIWebView * webView;
 @end
 
-@implementation LCWebViewViewController
+@implementation LCWebViewViewController{
+    NSString * _type;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = _titleStr;
     
-    UIWebView * webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth,ScreenHeight)];
-    if (_htmlStr) {
-       [webView loadHTMLString:_htmlStr baseURL:nil];
+    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, -45, ScreenWidth,ScreenHeight)];
+    if (!_htmlStr) {
+       [_webView loadHTMLString:_htmlStr baseURL:nil];
     }else{
-       [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
+       [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_urlStr]]];
     }
+    _webView.delegate = self;
     
-    [self.view addSubview:webView];
+    [self.view addSubview:_webView];
 
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.style.webkitUserSelect='none';"];
+    [webView stringByEvaluatingJavaScriptFromString:@"document.documentElement.style.webkitTouchCallout='none';"];
+    
+    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"WebKitCacheModelPreferenceKey"];
+    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"WebKitDiskImageCacheEnabled"];
+    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"WebKitOfflineWebApplicationCacheEnabled"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    [self handelRightDeil:webView];
+    
+}
+
+- (void)handelRightDeil:(UIWebView *)awebView {
+    NSString * javaScriptString = @"document.body.getElementsByClassName('header')[0].remove()";
+    _type =[awebView stringByEvaluatingJavaScriptFromString:javaScriptString];
 }
 
 @end
