f6836a3724d990c0985957e94fc5d4ef88a53886
xuzhou524 <xuuohz@163.com>
2019-04-15 16:34:05 +0800

    定位

--- LifeCollection/AppDelegate.m
+++ LifeCollection/AppDelegate.m
@@ -12,7 +12,15 @@
 #import "AddViewController.h"
 #import "FoundViewController.h"
 
-@interface AppDelegate ()
+#import <CoreLocation/CoreLocation.h>
+
+@interface AppDelegate ()<CLLocationManagerDelegate>{
+    CLLocationManager *locationmanager;//定位服务
+    NSString *strlatitude;//经度
+    NSString *strlongitude;//纬度
+}
+
+//@property(nonatomic,strong)NSString *currentCity;//当前城市
 
 @end
 
@@ -22,6 +30,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [LCColor backgroudColor];
+    
+    [self getLocation];
+    
     LCTabBarController * rootTabBarVC = [LCTabBarController new];
     self.window.rootViewController = rootTabBarVC;
     [LCClient sharedInstance].lcCenterTabBar = rootTabBarVC;
@@ -61,6 +72,62 @@
                                                                                 applicationActivities: applicationActivities];
         [[LCClient sharedInstance].lcCenterNav presentViewController:activityVC animated:TRUE completion:nil];
     }
+}
+
+-(void)getLocation{
+    //判断定位功能是否打开
+    if ([CLLocationManager locationServicesEnabled]) {
+        locationmanager = [[CLLocationManager alloc]init];
+        locationmanager.delegate = self;
+        [locationmanager requestAlwaysAuthorization];
+//        _currentCity = [NSString new];
+        [locationmanager requestWhenInUseAuthorization];
+
+        //设置寻址精度
+        locationmanager.desiredAccuracy = kCLLocationAccuracyBest;
+        locationmanager.distanceFilter = 5.0;
+        [locationmanager startUpdatingLocation];
+    }
+}
+
+#pragma mark CoreLocation delegate (定位失败)
+//定位失败后调用此代理方法
+//-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+//    //设置提示提醒用户打开定位服务
+//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"允许定位提示" message:@"请在设置中打开定位" preferredStyle:UIAlertControllerStyleAlert];
+//    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"打开定位" style:UIAlertActionStyleDefault handler:nil];
+//
+//    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+//    [alert addAction:okAction];
+//    [alert addAction:cancelAction];
+//    [self presentViewController:alert animated:YES completion:nil];
+//}
+
+#pragma mark 定位成功后则执行此代理方法
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    [locationmanager stopUpdatingHeading];
+    //旧址
+    CLLocation *currentLocation = [locations lastObject];
+    CLGeocoder *geoCoder = [[CLGeocoder alloc]init];
+    //打印当前的经度与纬度
+    NSLog(@"%f,%f",currentLocation.coordinate.latitude,currentLocation.coordinate.longitude);
+    
+    //反地理编码
+    [geoCoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (placemarks.count > 0) {
+            CLPlacemark *placeMark = placemarks[0];
+//            self.currentCity = placeMark.locality;
+//            if (!self.currentCity) {
+//                self.currentCity = @"无法定位当前城市";
+//            }
+            /*看需求定义一个全局变量来接收赋值*/
+            NSLog(@"----%@",placeMark.country);//当前国家
+//            NSLog(@"%@",self.currentCity);//当前的城市
+            NSLog(@"%@",placeMark.subLocality);//当前的位置
+            NSLog(@"%@",placeMark.thoroughfare);//当前街道
+            NSLog(@"%@",placeMark.name);//具体地址
+        }
+    }];
 }
 
 @end
