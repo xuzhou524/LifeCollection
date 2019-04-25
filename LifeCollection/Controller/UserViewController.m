dd52796f3a7936244c94be1884b979e402d85a67
xuzhou524 <xuuohz@163.com>
2019-04-25 15:07:55 +0800

    bak

--- LifeCollection/Controller/UserViewController.m
+++ LifeCollection/Controller/UserViewController.m
@@ -101,7 +101,7 @@
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.section == 0) {
         if (indexPath.row == 0) {
-            return 200;
+            return 210;
         }else{
             return 95;
         }
