2f9626c1bdfe1e3f0a63f340517040ee8c01030e
xuzhou524 <xuuohz@163.com>
2019-05-24 12:22:29 +0800

    add iMessage

---
+++ LifeCollectioniMessage MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  LifeCollectioniMessage MessagesExtension
+//
+//  Created by gozap on 2019/5/24.
+//  Copyright © 2019 com.longdai. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
